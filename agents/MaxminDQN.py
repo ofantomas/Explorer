b5d5811ebb5534d9bcc719411c936348b46085d5
ofantomas <tsypinartem9@gmail.com>
2021-12-28 18:05:57 +0300

    Autotune number of Q networks #1

--- agents/MaxminDQN.py
+++ agents/MaxminDQN.py
@@ -11,6 +11,11 @@ class MaxminDQN(VanillaDQN):
   def __init__(self, cfg):
     super().__init__(cfg)
     self.k = cfg['agent']['target_networks_num'] # number of target networks
+    self.nets_to_use = cfg['eta']['init_d']
+    self.update_d_iterval = cfg['eta']['update_d_interval']
+    self.update_d_gamma = cfg['eta']['update_d_gamma']
+    self.Q_G_delta = 0
+
     # Create k different: Q value network, Target Q value network and Optimizer
     self.Q_net = [None] * self.k
     self.Q_net_target = [None] * self.k
@@ -31,21 +36,32 @@ class MaxminDQN(VanillaDQN):
     if (self.step_count // self.cfg['network_update_frequency']) % self.cfg['target_network_update_frequency'] == 0:
       for i in range(self.k):
         self.Q_net_target[i].load_state_dict(self.Q_net[i].state_dict())
-
+  
   def compute_q_target(self, batch):
     with torch.no_grad():
-      q_min = self.Q_net_target[0](batch.next_state).clone()
-      for i in range(1, self.k):
-        q = self.Q_net_target[i](batch.next_state)
-        q_min = torch.min(q_min, q)
+      q_min = torch.min(torch.stack([self.Q_net_target[i](batch.next_state) for i in range(self.nets_to_use)], 1), 1)
       q_next = q_min.max(1)[0]
       q_target = batch.reward + self.discount * q_next * batch.mask
     return q_target
   
   def get_action_selection_q_values(self, state):
-    q_min = self.Q_net[0](state)
-    for i in range(1, self.k):
-      q = self.Q_net[i](state)
-      q_min = torch.min(q_min, q)
+    q_min = torch.min(torch.stack([self.Q_net_target[i](state) for i in range(self.nets_to_use)], 1), 1)
     q_min = to_numpy(q_min).flatten()
     return q_min
+
+  def update_d(self):
+    if self.Q_G_delta < 0:
+      self.nets_to_use = max(self.nets_to_use - 1, 1)
+    if self.Q_G_delta > 0:
+      self.nets_to_use = min(self.nets_to_use + 1, self.k)
+
+  def eval_thresholds(self, replay_buffer, n_per_episode):
+    res_uniform = self.eval_thresholds_by_type(replay_buffer, n_per_episode, 'uniform')
+    res_episodes = self.eval_thresholds_by_type(replay_buffer, n_per_episode, 'episodes')
+    res = dict()
+    res.update(res_uniform)
+    res.update(res_episodes)
+    last_Q_G_delta = res[f'LastReplay_{self.sampling_scheme}/Q_value_t={self.critic.n_quantiles}'] - \
+              res[f'LastReplay_{self.sampling_scheme}/Returns']
+    self.Q_G_delta = self.Q_G_delta * self.delta_gamma + last_Q_G_delta * (1 - self.delta_gamma)
+    return res
